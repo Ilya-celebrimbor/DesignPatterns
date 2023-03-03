e745b2898f432702ff92a71c9754c03c3bece2de
Ilya-Celembrimbor <kanishevilya2@gmail.com>
2023-03-03 19:46:15 +0600

    first commit

---
+++ ClassWork1_2/src/Creational/Singleton/Main.java
@@ -0,0 +1,9 @@
+package Creational.Singleton;
+
+public class Main {
+    //
+    public static void main(String[] args){
+        singleton single = singleton.getInstance();
+        single.getDescription();
+    }
+}
